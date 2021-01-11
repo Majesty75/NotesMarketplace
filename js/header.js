c0cd603a0dc84b5c0240d65905cf29b8c0152352
Yash Goyani <yashgoyani101@gmail.com>
2021-01-11 22:02:59 +0530

    html-submission-1

---
+++ js/header.js
@@ -0,0 +1,55 @@
+/* ------ Show and Hide Navigation ------ */
+
+$(function () {
+
+    //change nav on load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        //change nav on scroll
+        showHideNav();
+
+    });
+
+    function showHideNav(isMobile = false) {
+        
+        if ($(window).scrollTop() > 20 || isMobile) {
+            //show white navigation bar
+            $('header').addClass('sticky-header');
+            $('.logo-wrapper img').attr('src', '../imgs/icons/logo.png');
+            $('.navigation-wrapper li button').removeClass('white-btn');
+            $('.navigation-wrapper li button').addClass('rblue-btn');
+            if(!$('#mobile-navigation-wrapper').is(':visible')){
+                $('.menu-btn').find('img').attr('src', './imgs/icons/bars-solid-rblue.png');
+            } else {
+                $('.menu-btn').find('img').attr('src', './imgs/icons/times-solid-rblue.png');
+            }
+
+        } else {
+
+            //hide white navigation bar
+            $('header').removeClass('sticky-header');
+            $('.logo-wrapper img').attr('src', '../imgs/icons/top-logo.png');
+            $('.navigation-wrapper li button').removeClass('rblue-btn');
+            $('.navigation-wrapper li button').addClass('white-btn');
+            $('.menu-btn').find('img').attr('src', './imgs/icons/bars-solid-white.png');
+        }
+
+    }
+    
+    $('.menu-btn').on('click',function() {
+        
+        $('#mobile-navigation-wrapper').slideToggle(200, function() {
+            
+            if(!$('header').hasClass('sticky-header')){
+                showHideNav(true);
+            } else {
+            showHideNav(false);
+            }
+            
+        });
+        $('body').toggleClass('disable-scroll');
+    });
+
+});
