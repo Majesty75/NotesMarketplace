74ac20f96f3dafd1d6327b28bcc28acac6e47fe2
Yash Goyani <yashgoyani101@gmail.com>
2021-03-27 12:46:02 +0530

    Bug Fixes mobile menu , charset, input font size

--- js/script.js
+++ js/script.js
@@ -46,7 +46,6 @@ $(function() {
         
         if(!target.is(':visible')){
             //hide all other contex menus
-            console.log('I hate ASP.NET');
             $('.dropdown-menu').slideUp();
         }
         target.slideToggle();
